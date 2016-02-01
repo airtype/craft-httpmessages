98abf26ee22d6374a4aec55e1514639ef0bdd1e4
Wes Rice <wes@airtype.com>
2016-02-01 16:05:50 -0500

    Added conditional wrappers and mapping to \Craft\craft()

--- functions.php
+++ functions.php
@@ -1,6 +1,16 @@
 <?php
 
-function dd($mixed)
-{
-    \Craft\Craft::dd($mixed);
+if (!function_exists('dd')) {
+    function dd($mixed)
+    {
+        \Craft\Craft::dd($mixed);
+    }
 }
+
+if (!function_exists('craft')) {
+    function craft()
+    {
+        return \Craft\craft();
+    }
+}
+
