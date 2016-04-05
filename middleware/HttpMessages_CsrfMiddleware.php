9e10a3eb643dfd24596e145b38731b091a93ad44
Wes Rice <wes@airtype.com>
2016-04-05 16:48:42 -0400

    Added Csrf middleware

---
+++ middleware/HttpMessages_CsrfMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Craft;
+
+use Craft\HttpMessages_CraftRequest as Request;
+use Craft\HttpMessages_CraftResponse as Response;
+
+class HttpMessages_CsrfMiddleware
+{
+    /**
+     * __invoke Magic Method
+     *
+     * @param Request  $request  Request
+     * @param Response $response Response
+     * @param callable $next     Next
+     *
+     * @return Response Response
+     */
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        $controller = $request->getRoute()->getController();
+        $method = $request->getRoute()->getMethod();
+
+        $controller = new $controller();
+
+        $response = $controller->$method($request, $response);
+
+        return $response;
+    }
+}
